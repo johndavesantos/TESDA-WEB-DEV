53982f1aa0f05595aa646d889facea9a26d421b8
johndavesantos <https://github.com/johndavesantos/TESDA-WEB-DEV.git>
2021-02-05 13:56:22 +0800

    Push to github -initial commit

---
+++ L01ASS/app.js
@@ -0,0 +1,28 @@
+// Log to console
+
+console.log('Hello everyone');
+console.log(123);
+console.log(true);
+var greeting = 'Hello';
+console.log(greeting);
+greeting = 'How are you';
+console.log(greeting);
+console.log([1,2,3,4,5]);
+
+console.log({a: 1, b: 2});
+console.table({a: 1, b: 2});
+
+console.error('This is some error');
+console.clear();
+console.warn('This is a warning');
+console.time('Hello');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.log('Hello world');
+console.timeEnd('Hello');
+
+//
+ 
